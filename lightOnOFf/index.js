9771a78b31672831b140f1db75f2ae8e592035ea
nitinrajputji <nitinrajput12911@gmail.com>
2021-12-12 19:13:41 +0530

    'initial'

---
+++ lightOnOFf/index.js
@@ -0,0 +1,7 @@
+// const lightOn = (event) => {
+//   console.log("hel");
+//   let changeColor = event.target;
+
+//   console.log(changeColor);
+//   changeColor.setAttribute("src", "./image/th.jpg");
+// };
